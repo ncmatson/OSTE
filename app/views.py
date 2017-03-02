3bc829d3be51631b5b77df22f1117824422d39df
Cameron Matson <cmatson@smu.edu>
2017-03-01 23:28:45 -0600

    initial commit

---
+++ app/views.py
@@ -0,0 +1,36 @@
+from app import app, grabber
+from flask import render_template, request, url_for
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import os, re
+
+@app.route('/')
+@app.route('/index')
+def home():
+    return render_template('index.html')
+
+def rm(dir, pattern):
+    for f in os.listdir(dir):
+        if re.search(pattern, f):
+            os.remove(os.path.join(dir, f))
+
+def edgeit(img):
+    i = cv2.imread(img)
+    edges = cv2.Canny(i,100,200)
+    cv2.imwrite(img, edges)
+
+@app.route('/grabber/', methods=['POST'])
+def doGrabber():
+    rm('app/static/img', 'dg*')
+    data = request.form
+    lat = data['lat']
+    lon = data['lon']
+    zoom = data['zoom']
+
+    g = grabber.Grabber('app/static/img')
+    time = g.grab(lat, lon, zoom)
+    edgeit('app/static/img/dg'+time+'.jpg')
+
+    url = url_for('static', filename='img/dg'+time+'.jpg')
+    return url
