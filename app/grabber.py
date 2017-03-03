91a6e98b53aeb8b7871f817a8702b4daaf0b5fff
Cameron Matson <cmatson@smu.edu>
2017-03-03 13:20:54 -0600

    remove access key from grabber

---
+++ app/grabber.py
@@ -0,0 +1,32 @@
+import urllib.request
+import time
+
+class Grabber():
+    """grabber sets up the api to grb images from the Digital globe
+    site"""
+    def __init__(self, dest, token, form='jpg'):
+        self.time = str(time.time())
+        self.token = token
+        self.map_id = 'digitalglobe.nal0g75k'
+
+        # location to store the grabbed images
+        self.dest = dest
+
+        # type of images to grab either (jpg or png)
+        if form == 'png':
+            self.ext = '.png'
+            self.form = 'png256'
+        else:
+            self.ext = '.jpg'
+            self.form = 'jpg90'
+
+    def grab(self, lat, lon, zoom, size='512x512'):
+        # name = str(lat) + '_' + str(lon)
+        name = 'dg'+self.time
+        loc = str(lon) + ',' + str(lat) + ',' + str(zoom)
+
+        path = self.dest + '/' + name + self.ext
+        url = 'https://api.mapbox.com/v4/'+self.map_id+'/'+loc+'/'+size+'.'+self.form+'?access_token='+self.token
+        urllib.request.urlretrieve(url, path)
+
+        return self.time
