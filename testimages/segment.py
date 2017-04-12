55676d44db289ee44f7bde220a748425a7572bac
Cameron Matson <cmatson@smu.edu>
2017-04-12 16:23:41 -0500

    center of buildings (and fun colors)

--- testimages/segment.py
+++ testimages/segment.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import random
 
 img = cv2.imread('dg1490743858.59531.png')
 Bchan,Gchan,Rchan = cv2.split(img)
@@ -47,8 +48,25 @@ out[markers==-1] = [255,0,0]
 cv2.imwrite('greendisttransform.png',out)
 
 #try findContours
-im2, contours, hierarchy = cv2.findContours(binimg, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_L1)
+im2, contours, hierarchy = cv2.findContours(binimg, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+print(len(contours))
 out2 = np.zeros(img.shape,dtype=np.uint8)
 out2[binimg == 255] = [0,0,255]
 cv2.drawContours(out2,contours, -1, (0,255,255), 3)
 cv2.imwrite('contours.png',out2)
+
+# find the moments of each object
+# M = np.zeros(len(contours))
+for c in contours:
+    r = random.randint(20,200)
+    g = random.randint(20,200)
+    b = random.randint(20,200)
+
+    cv2.fillPoly(out2, pts=[c], color=[r, g, b])
+    m = cv2.moments(c)
+    if (m['m10'] > 0):
+        cx = int(m['m10']/m['m00'])
+        cy = int(m['m01']/m['m00'])
+        cv2.circle(out2, (cx,cy), 5, [255,255, 255])
+
+cv2.imwrite('contours.png', out2)
