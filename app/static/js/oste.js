b86fdaa54ec3872ba434d93ad3b492368a28d4d5
Cameron Matson <cmatson@smu.edu>
2017-04-26 21:44:52 -0500

    show dg and drone image and their areas in app

--- app/static/js/oste.js
+++ app/static/js/oste.js
@@ -2,7 +2,7 @@ var map;
 var ratio;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 32.8451799255302, lng: -96.78439629983905},
+    center: {lat: 32.84468416978932, lng: -96.78363991689685},
     zoom: 19,
     tilt: 0,
     mapTypeId: 'satellite',
@@ -38,15 +38,23 @@ $("#edgebutton").on('click', function(){
     // set images
 
     $("#dumy").attr('src',result.url_dumb); // contours of unprocessed image
+    $("#drone").attr('src', result.url_drone);
 
     // clear the HTML listing the areas
-    $("#areas").html("");
+    $("#drone_areas").html("");
+    $("#dumb_areas").html("");
 
     // write the area for each contour to a table
-    for (i = 0; i < result.areas.length; ++i){
+    for (i = 0; i < result.dumb_areas.length; ++i){
       console.log('the ratio is ', ratio);
-      var area = "<tr><td>"+(i)+"</td><td>"+result.areas[i]*Math.pow(ratio,2)+"</td></tr>";
-      $("#areas").append(area);
+      var area = "<tr><td>"+(i)+"</td><td>"+result.dumb_areas[i]*Math.pow(ratio,2)+"</td></tr>";
+      $("#dumb_areas").append(area);
+    }
+
+    for (i = 0; i < result.drone_areas.length; ++i){
+      console.log('the ratio is ', ratio);
+      var area = "<tr><td>"+(i)+"</td><td>"+result.drone_areas[i]*Math.pow(ratio,2)+"</td></tr>";
+      $("#drone_areas").append(area);
     }
   }, "json");
 });
--- app/views.py
+++ app/views.py
@@ -26,11 +26,41 @@ def doGrabber():
     lon = data['lon']
     zoom = data['zoom']
 
-    # use the drone image
-    dumb_contours = segment.dumb_contours('app/static/img/stills/small/small_dallas.png', 'app/static/img/dumy_dallas2.png')
+    with open('app/static/secrets.txt') as f: token = f.read()
+
+    # get the location from digital globe
+    g = grabber.Grabber('app/static/img', token,'png')
+    time = g.grab(lat, lon, zoom, '832x469')
+
+    # # 'smart' means that the image went through the neural net prediction script
+    # smart_contours = segment.predict(time,'app/ma_prediction_400/dg%s.png'%(time), 'app/static/img/nn_dg'+time+'.png')
+    # smart_areas = segment.get_areas(smart_contours.values())
+    #
+    # # 'dumb' meanas that the segmentation was on the original image
+    # dumb_contours = segment.dumb_contours('app/static/img/dg'+time+'.png','app/static/img/dumy_dg'+time+'.png')
+    # dumb_areas = segment.get_areas(dumb_contours.values())
+    #
+    # # uses 'smart' locations to pick out contours in the 'dumb' image
+    # buildings = merge.intersect(smart_contours, dumb_contours)
+    # merge.mkimage('app/static/img/dg'+time+'.png','app/static/img/merge_dg'+time+'.png', buildings)
+    # areas = segment.get_areas(buildings.values())
+    #
+    # url_nn = url_for('static', filename='img/nn_base_dg'+time+'.png')
+    # url_smart = url_for('static', filename='img/nn_dg'+time+'.png')
+    # url_dumb = url_for('static', filename='img/dumy_dg'+time+'.png')
+    # url_merge = url_for('static', filename='img/merge_dg'+time+'.png')
+
+    # for cameron
+    dumb_contours = segment.dumb_contours('app/static/img/dg'+time+'.png','app/static/img/dumy_dg'+time+'.png')
     dumb_areas = segment.get_areas(dumb_contours.values())
 
-    areas = dumb_areas
-    url_dumb = url_for('static', filename='img/dumy_dallas2.png')
+    drone_contours = segment.dumb_contours('app/static/img/stills/small/small_hyer.png', 'app/static/img/cont_hyer.png')
+    drone_areas = segment.get_areas(drone_contours.values())
 
-    return jsonify(url_dumb=url_dumb, areas=areas)
+
+    url_dumb = url_for('static', filename='img/dumy_dg'+time+'.png')
+    url_drone = url_for('static', filename='img/cont_hyer.png')
+
+    return jsonify(url_drone=url_drone, url_dumb=url_dumb,
+                   drone_areas=drone_areas, dumb_areas=dumb_areas,
+                   )
